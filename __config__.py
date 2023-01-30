446ce033c055a7df3c3bc8bb84d17a16fea93164
Efthymios Tzinis <etzinis2@illinois.edu>
2023-01-29 20:02:18 -0600

    Update __config__.py

--- __config__.py
+++ __config__.py
@@ -1,4 +1,4 @@
 LIBRI3MIX_ROOT_PATH = '/mnt/data/libri_mix/Libri3Mix'
 CHiME_ROOT_PATH = '/mnt/data/CHIME/processed_CHiME_10s/'
 
-API_KEY = 'RJlDCKh8TVXGJTG3ChR3GaY6s'
+API_KEY = 'insert_your_cometml_API_key_here'
