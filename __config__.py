82fc6ee5107c722d3e8c4ddafc4cdd1611b5980d
Léonie Borne <leonie.borne@gmail.com>
2023-02-02 17:54:55 +0100

    remove comet api key

--- __config__.py
+++ __config__.py
@@ -2,4 +2,4 @@ LIBRI3MIX_ROOT_PATH = '/mnt/data/libri_mix/Libri3Mix'
 CHiME_ROOT_PATH = '/mnt/data/CHIME/processed_CHiME_10s/'
 LIBRICHiME_ROOT_PATH = '/mnt/data/CHIME/LibriCHiME5/'
 
-API_KEY = 'NT9yG3QYnEXRzBOiMOpVEspW1'
+API_KEY = 'insert_your_cometml_API_key_here'
